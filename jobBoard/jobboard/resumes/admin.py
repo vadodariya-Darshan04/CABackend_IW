9c3f2c02bac6eb5b040ba698280e753745245659
Darshan Vadodariya <drvadodariya10@gmail.com>
2026-05-11 18:49:42 +0530

    Code redability improved

--- jobBoard/jobboard/resumes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Resume
-
-
-@admin.register(Resume)
-class ResumeAdmin(admin.ModelAdmin):
-    list_display = ['title', 'candidate', 'is_default', 'uploaded_at']
-    list_filter = ['is_default']
-    search_fields = ['candidate__user__email', 'title']
--- jobBoard/jobboard/resumes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ResumeListCreateView.as_view(), name='resume_list'),
-    path('<int:pk>/', views.ResumeDetailView.as_view(), name='resume_detail'),
-    path('<int:pk>/set-default/', views.SetDefaultResumeView.as_view(), name='set_default_resume'),
-]
